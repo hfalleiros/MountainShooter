22f1915d86641c75c01378c22332cf82f53b560d
Henrique Falleiros <henrique.falleiros@gmail.com>
2025-06-23 23:33:51 -0300

    Including Assets, adding sound and the menu text.

--- code/game.py
+++ code/game.py
@@ -13,15 +13,7 @@ class Game:
 
     def run(self):
 
-
-
-
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            #for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()
-            #        quit()
