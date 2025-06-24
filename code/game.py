2f351b8f1de7c8e8c0304ee5ccd61413277b3641
Henrique Falleiros <henrique.falleiros@gmail.com>
2025-06-23 22:18:28 -0300

    Creating main window with background. Creating const.py fila

--- code/game.py
+++ code/game.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 from code.menu import Menu
+from code.const import WIN_WIDTH
+from code.const import WIN_HEIGHT
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
+
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
